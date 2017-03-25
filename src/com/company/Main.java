3e463fb525adb715d0ca11f29fbe9ef766060d6c
Danas Danas <Danas Danas>
2017-03-25 21:12:24 +0200

    InputValidator class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
 
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+    	
+    	InputValidator validuojam = new InputValidator();
+    	System.out.println(validuojam.validateString("input.txt"));
+    	//System.out.println(validuojam.getStr());
+    	
+    	
 	// write your code here
     }
 }
